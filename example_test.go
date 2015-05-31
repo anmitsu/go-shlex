ca38a87b65c0280c359878cbb3e307d76f9c6541
anmitsu <anmitsu.s@gmail.com>
2015-05-31 14:49:13 +0900

    first commit

---
+++ example_test.go
@@ -0,0 +1,54 @@
+package shlex_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/anmitsu/go-shlex"
+)
+
+func ExampleSplit() {
+	cmd := `cp -Rdp "file name" 'file name2' dir\ name`
+
+	words1, err := shlex.Split(cmd, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	words2, err := shlex.Split(cmd, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Source command:")
+	fmt.Println(`cp -Rdp "file name" 'file name2' dir\ name`)
+	fmt.Println()
+
+	fmt.Println("POSIX mode:")
+	for _, word := range words1 {
+		fmt.Println(word)
+	}
+	fmt.Println()
+	fmt.Println("Non-POSIX mode:")
+	for _, word := range words2 {
+		fmt.Println(word)
+	}
+
+	// Output:
+	// Source command:
+	// cp -Rdp "file name" 'file name2' dir\ name
+	//
+	// POSIX mode:
+	// cp
+	// -Rdp
+	// file name
+	// file name2
+	// dir name
+	//
+	// Non-POSIX mode:
+	// cp
+	// -Rdp
+	// "file name"
+	// 'file name2'
+	// dir\
+	// name
+}
